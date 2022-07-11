0b1563d9c062faed62865b0531ea5856f3981cdc
long <longnguyen176251@gmail.com>
2022-07-10 23:03:57 -0500

    disable rainbow on js and html

--- lua/config.lua
+++ lua/config.lua
@@ -108,15 +108,16 @@ g.bracey_refresh_on_save = true
 -----------------------
 -- TREESITTER CONFIG --
 -----------------------
+local ts_visual_disable = { "typescript", "tsx", "html", "javascript" }
 require("nvim-treesitter.configs").setup({
     ensure_installed = "all",
     highlight = {
         enable = true,
-        disable = { "typescript", "tsx", "html", "javascript" },
+        disable = ts_visual_disable,
     },
     rainbow = {
         enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+        disable = ts_visual_disable,
         extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
         max_file_lines = nil, -- Do not enable for files with more than n lines, int
         -- colors = {}, -- table of hex strings
